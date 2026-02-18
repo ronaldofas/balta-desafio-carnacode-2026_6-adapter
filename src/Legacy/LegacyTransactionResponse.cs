81bc1574c0c85f615b87978ea8c893de361c2e78
Ronaldo Silva <ronaldofas@gmail.com>
2026-02-18 00:00:57 -0300

    feat: extract legacy payment system classes

---
+++ src/Legacy/LegacyTransactionResponse.cs
@@ -0,0 +1,9 @@
+namespace DesignPatternChallenge.Legacy;
+
+public class LegacyTransactionResponse
+{
+    public string AuthCode { get; set; }
+    public string ResponseCode { get; set; }
+    public string ResponseMessage { get; set; }
+    public string TransactionRef { get; set; }
+}
